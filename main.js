d615517ee2588c835cb53734aa44720430532545
AhmedY29 <ahmedys2001@gmail.com>
2025-05-08 10:01:31 +0300

    complete task2

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let fontSizeUp = document.getElementById("fontSizeUp");
 let fontSizeDown = document.getElementById("fontSizeDown");
 let ChangeTextColor = document.getElementById("ChangeTextColor");
 let ChangeBg = document.getElementById("ChangeBg");
+let addImg = document.getElementById("addImg");
 
 changeText.addEventListener("click", () => {
   h1Text.innerText = "Ahmed Alsaleh";
@@ -20,3 +21,15 @@ ChangeTextColor.addEventListener("click", () => {
 ChangeBg.addEventListener("click", () => {
   document.body.style.backgroundColor = "#f2f2f2";
 });
+
+let img = document.createElement("img");
+addImg.addEventListener("click", () => {
+  if (img.src == "") {
+    img.src =
+      "https://images.pexels.com/photos/13422162/pexels-photo-13422162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  } else {
+    img.src =
+      "https://images.pexels.com/photos/30921109/pexels-photo-30921109/free-photo-of-majestic-icelandic-waterfall-in-scenic-landscape.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  }
+});
+document.body.appendChild(img);
